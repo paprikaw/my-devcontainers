cd64dbdb4d15cf95e893dcd61df85d4b06049f7c
baixu.must@gmail.com <baixu.must@gmail.com>
2023-02-01 09:08:57 +0000

    update script to adapt more freatures for NJU container

---
+++ NJU-OS/boot.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# add scripts to path
+
+cp /workspaces/NJU_OS/my-devcontainers/NJU-OS/.profile ~/.profile
+# Install powerline for zsh
+wget https://raw.githubusercontent.com/paprikaw/rc_files/main/script/install_zsh.sh 
+bash install_zsh.sh 
+rm install_zsh.sh
+
+# configure git
+git config --global user.email "baixu.must@gmail.com"
+git config --global user.name "baixu.must@gmail.com"
