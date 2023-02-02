17ced7045f1d47bd5f909ccc0af34a77d577bbff
Bai Xu <baixu.must@gmail.com>
2023-02-02 03:49:10 +0000

    update

--- NJU-OS/boot.sh
+++ NJU-OS/boot.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
 # add scripts to path
+touch ~/.profile
+cat /workspaces/NJU_OS/my-devcontainers/NJU-OS/.profile >> ~/.profile
 
-cp /workspaces/NJU_OS/my-devcontainers/NJU-OS/.profile ~/.profile
 # Install powerline for zsh
 wget https://raw.githubusercontent.com/paprikaw/rc_files/main/script/install_zsh.sh 
 bash install_zsh.sh 
 rm install_zsh.sh
 
 # configure git
-git config --global user.email "baixu.must@gmail.com"
-git config --global user.name "baixu.must@gmail.com"
+# git config --global user.email "baixu.must@gmail.com"
+# git config --global user.name "baixu.must@gmail.com"
